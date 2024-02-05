aa64a98aa7812705d0dde35e734f1b29b48b0922
AhmedNasser1010 <ahmed.mix.games@gmail.com>
2024-02-05 20:12:17 +0200

    delete some files

--- src/function/getIpAddress.js
+++
@@ -1,8 +0,0 @@
-const getIpAddress = () => {
-    return fetch('https://api64.ipify.org?format=json')
-      .then(response => response.json())
-      .then(data => data.ip)
-      .catch(error => console.error('Error fetching IP:', error));
-}
-
-export default getIpAddress;
--- src/validation_schemas/businessPaymentMethodsValidationSchema.js
+++
@@ -1,9 +0,0 @@
-import { object, boolean } from 'yup';
-
-const businessPaymentMethodsValidationSchema = object({
-	cash: boolean(),
-	vodafoneCash: boolean(),
-	etisalatCash: boolean(),
-});
-
-export default businessPaymentMethodsValidationSchema;
