34a04b1bdd6c4c3c5fc6b2ecab0e4897323ddfb2
AhmedNasser1010 <ahmed.mix.games@gmail.com>
2024-02-07 22:33:24 +0200

    .

--- business_manager/src/Component/CategoryBody.jsx
+++ business_manager/src/Component/CategoryBody.jsx
@@ -2,73 +2,44 @@ import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Box from '@mui/material/Box';
 import {
-	arrayMove,
   SortableContext,
-  sortableKeyboardCoordinates,
   verticalListSortingStrategy
 } from '@dnd-kit/sortable';
-import {
-	DndContext,
-  KeyboardSensor,
-  PointerSensor,
-  useSensor,
-  useSensors,
-  closestCenter,
-} from '@dnd-kit/core';
 import SortableItem from './SortableItem.jsx';
+import { v4 as uuidv4 } from 'uuid';
 
 const CategoryBody = ({ category }) => {
 	const items = useSelector(state => state.menu.items);
 	const [filteredItems, setFilteredItems] = useState([]);
-	const [indexes, setIndexes] = useState([1, 2, 3]);
+	const [indexes, setIndexes] = useState([]);
+	const uuid = uuidv4();
 
 	// setup filtered items by filter all items with spicific category
 	useEffect(() => {
-		setFilteredItems(prevItems => {
-			items.filter(item => item.category === category)
-		})
-	}, [])
+		let result = [];
+		items.map(item => item.category === category && result.push(item));
+		setFilteredItems(result);
+	}, [items])
 
 	// setup indexed from categories
 	useEffect(() => {
-		!indexes.length && setIndexes(prevIndexes => filteredItems?.map((item, i) => i + 1));
+		!indexes.length && setIndexes(prevIndexes => filteredItems?.map((item, i) => i + 1 + uuid));
 	}, [filteredItems])
 
 	useEffect(() => {
-		console.log(filteredItems);
-	}, [filteredItems])
-
-	const sensors = useSensors(
-    useSensor(PointerSensor),
-    useSensor(KeyboardSensor, {
-      coordinateGetter: sortableKeyboardCoordinates,
-    })
-  );
-
-	const handleDragEnd = (event) => {
-    const {active, over} = event;
-    
-    if (active.id !== over.id) {
-      setIndexes((indexes) => {
-        const oldIndex = indexes.indexOf(active.id);
-        const newIndex = indexes.indexOf(over.id);
-        
-        return arrayMove(indexes, oldIndex, newIndex);
-      });
-    }
-  }
+		console.log(indexes)
+	}, [indexes])
 
 	return (
 
 		<Box
 			sx={{
-				height: '500px',
 			}}
 		>
 			<SortableContext items={indexes} strategy={verticalListSortingStrategy}>
-				{indexes?.map(index =>
+				{indexes?.map((index, i) =>
 					<SortableItem key={index} id={index} >
-						<span>{ filteredItems[index - 1] }</span>
+						<span>{ filteredItems[i].title }</span>
 					</SortableItem>
 				)}
 			</SortableContext>
