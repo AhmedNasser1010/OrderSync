762c6fdfb027c8c0f0295eabca907c8a6f0d00d5
AhmedNasser1010 <ahmed.mix.games@gmail.com>
2024-07-21 14:26:31 +0300

    [Bug] The auto mode is never waiting for the drivers who is out of range leading to errors

--- orders_distributor_app/utils/findDriversWithinDistance.js
+++ orders_distributor_app/utils/findDriversWithinDistance.js
@@ -1,34 +1,65 @@
 const { performance } = require('perf_hooks')
 const getDistanceFromLatlngInKm = require('./getDistanceFromLatlngInKm')
-const handleNoDriverWithinDistance = require('./handleNoDriverWithinDistance')
 const { debuggingMode, distances } = require('../constants.js')
+const { onSnapshot, collection } = require("firebase/firestore")
+const { db } = require("../config/firebase.js")
 
-function findDriversWithinDistance(order, drivers, resLocation) {
-	const start = performance.now()
+function findDriversNearestDrivers(drivers, resLocation) {
+  const nearest = []
+
+  for (const distance of distances) {
+    if (drivers.length > 0) {
+      for (const driver of drivers) {
+        const driverDistance = getDistanceFromLatlngInKm(driver.liveLocation, resLocation)
+        driverDistance <= distance && nearest.push(driver)
+      }
+    }
+
+    if (nearest.length) return { drivers: nearest, distance }
+  }
 	
-	const closestDrivers = []
+	return { drivers: nearest, distance: distances[distances.length-1] }
+}
 
-	for (const distance of distances) {
-
-		if (drivers.length > 0) {
-			for (const driver of drivers) {
-
-				const driverDistance = getDistanceFromLatlngInKm(driver.liveLocation, resLocation)
-				driverDistance <= distance && closestDrivers.push(driver)
-
-			}
-		}
-
-		if (closestDrivers.length) {
+async function findDriversWithinDistance(order, drivers, resLocation) {
+	try {
+		const start = performance.now()
+	
+		const nearestDrivers = findDriversNearestDrivers(drivers, resLocation)
+		
+		if (nearestDrivers.drivers.length) {
 			const end = performance.now()
-			debuggingMode && console.log(`PASSED  6 ${(end - start).toFixed(2)}ms`)
-
-			return { drivers: closestDrivers, distance }
+      debuggingMode && console.log(`PASSED  6 ${(end - start).toFixed(2)}ms`)
+			return nearestDrivers
 		}
 
-	}
+		console.log("ASSIGN: Waiting for online or drivers within specific distance...")
+		return new Promise((resolve, reject) => {
+      const unsub = onSnapshot(collection(db, "drivers"), querySnapshot => {
+        const drivers = []
+        
+        querySnapshot.forEach(doc => {
+          drivers.push(doc.data())
+        })
+        
+        const nearestDrivers = findDriversNearestDrivers(drivers, resLocation)
+        
+        if (nearestDrivers.drivers.length) {
+          unsub()
+          
+          const end = performance.now()
+          debuggingMode && console.log(`PASSED  6 ${(end - start).toFixed(2)}ms`)
 
-	handleNoDriverWithinDistance(order, resLocation)
+          resolve(nearestDrivers)
+        }
+      }, reject)
+    })
+
+	} catch(e) {
+		console.log('Error while find drivers within distance:')
+		console.log(e)
+		throw e
+	}
 }
 
 module.exports = findDriversWithinDistance
