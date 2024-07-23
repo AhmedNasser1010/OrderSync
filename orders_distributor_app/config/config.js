c867d1edd1d51f61d915b1e59c92b31da8b5e0a3
AhmedNasser1010 <ahmed.mix.games@gmail.com>
2024-07-23 18:43:40 +0300

    update variable names

--- orders_distributor_app/config/config.js
+++ orders_distributor_app/config/config.js
@@ -4,28 +4,31 @@ const assert = require('assert')
 dotenv.config()
 
 const {
-  API_KEY,
-  AUTH_DOMAIN,
+  FIREBASE_API,
+  FIREBASE_AUTHDOMAIN,
   PROJECT_ID,
   STORAGE_BUCKET,
-  MESSAGING_SENDER_ID,
+  MESS_SEND_ID,
   APP_ID,
+  MEASUREMENT_ID
 } = process.env
 
-assert(API_KEY, 'API_KEY required')
-assert(AUTH_DOMAIN, 'AUTH_DOMAIN is required')
+assert(FIREBASE_API, 'FIREBASE_API required')
+assert(FIREBASE_AUTHDOMAIN, 'FIREBASE_AUTHDOMAIN is required')
 assert(PROJECT_ID, 'PROJECT_ID is required')
 assert(STORAGE_BUCKET, 'STORAGE_BUCKET is required')
-assert(MESSAGING_SENDER_ID, 'MESSAGING_SENDER_ID is required')
+assert(MESS_SEND_ID, 'MESS_SEND_ID is required')
 assert(APP_ID, 'APP_ID is required')
+assert(MEASUREMENT_ID, 'MEASUREMENT_ID is required')
 
 module.exports = {
   firebaseConfig: {
-    apiKey: API_KEY,
-    authDomain: AUTH_DOMAIN,
+    apiKey: FIREBASE_API,
+    authDomain: FIREBASE_AUTHDOMAIN,
     projectId: PROJECT_ID,
     storageBucket: STORAGE_BUCKET,
-    messagingSenderId: MESSAGING_SENDER_ID,
+    messagingSenderId: MESS_SEND_ID,
     appId: APP_ID,
+    measurementId: MEASUREMENT_ID
   },
 }
