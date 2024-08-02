a1d9be04f1965be5b1aa43f24e4ff75bd66ce2c4
AhmedNasser1010 <ahmed.mix.games@gmail.com>
2024-08-02 18:54:05 +0300

    stop service workers for customer app

--- customer_app/src/main.jsx
+++ customer_app/src/main.jsx
@@ -51,17 +51,17 @@ ReactDOM.createRoot(document.getElementById('root'))
 
 
 // Register the service worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-    .then(registration => {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    })
-    .catch(error => {
-      console.log('ServiceWorker registration failed: ', error);
-    });
-  });
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/service-worker.js')
+//     .then(registration => {
+//       console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//     })
+//     .catch(error => {
+//       console.log('ServiceWorker registration failed: ', error);
+//     });
+//   });
+// }
 
 window.read = 0
 window.write = 0
