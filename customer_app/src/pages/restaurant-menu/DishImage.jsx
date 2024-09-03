85449fe785b4b85b8058b1209e0a435fabfb02bc
ahmedn.coder <ahmedn.coder@gmail.com>
2024-09-03 06:51:55 +0000

    Use empty food image design instead of remove the img tag

--- customer_app/src/pages/restaurant-menu/DishImage.jsx
+++ customer_app/src/pages/restaurant-menu/DishImage.jsx
@@ -1,17 +1,17 @@
+import foodAndDrinkDesign from '../../../public/assets/FoodAndDrinkDesign.svg'
+
 const DishImage = ({ item }) => {
   return (
     <div className="dish-image-container">
-      {item?.backgrounds[0] && (
-        <button className="cursor-pointer w-[118px] h-24 rounded-md">
-          <img
-            src={item?.backgrounds[0]}
-            alt="menu-img"
-            className="rounded-md w-[118px] h-24 object-cover"
-          />
-        </button>
-      )}
+      <button className="cursor-pointer w-[118px] h-24 rounded-md">
+        <img
+          src={item?.backgrounds[0] || foodAndDrinkDesign}
+          alt="menu-img"
+          className="rounded-md w-[118px] h-24 object-cover"
+        />
+      </button>
     </div>
-  );
-};
+  )
+}
 
-export default DishImage;
+export default DishImage
