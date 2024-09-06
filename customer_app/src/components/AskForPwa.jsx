5898f8bc664ac7f25b5c2193ee96ea3920a7cc37
ahmedn.coder <ahmedn.coder@gmail.com>
2024-09-06 16:05:57 +0000

    reverse isOpen

--- customer_app/src/components/AskForPwa.jsx
+++ customer_app/src/components/AskForPwa.jsx
@@ -15,7 +15,7 @@ const AskForPwa = () => {
   return (
     <div
       className={`flex underline select-none align-center justify-center fixed w-full bottom-0 left-0 font-ProximaNovaBold text-center text-white p-1 bg-color-2 duration-300 translate-y-full opacity-0 ${
-        isOpen ? '!translate-y-0 opacity-100' : 'translate-y-full opacity-0'
+        !isOpen ? '!translate-y-0 opacity-100' : 'translate-y-full opacity-0'
       } delay-[5000ms] cursor-pointer`}
       onMouseUp={askForPwa()}>
       <MdInstallMobile />
