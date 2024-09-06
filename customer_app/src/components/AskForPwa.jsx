adc864f95234365e55abd65b3d762bb266d256b4
ahmedn.coder <ahmedn.coder@gmail.com>
2024-09-06 16:24:57 +0000

    .

--- customer_app/src/components/AskForPwa.jsx
+++ customer_app/src/components/AskForPwa.jsx
@@ -9,15 +9,15 @@ const AskForPwa = () => {
   const { t } = useTranslation()
 
   useEffect(() => {
-    window.matchMedia('(display-mode: standalone)').matches ? setIsOpen(true) : setIsOpen(false)
+    window.matchMedia('(display-mode: standalone)').matches ? setIsOpen(false) : setIsOpen(true)
   }, [])
 
   return (
     <div
-      className={`flex underline select-none align-center justify-center fixed w-full bottom-0 left-0 font-ProximaNovaBold text-center text-white p-1 bg-color-2 duration-300 translate-y-full opacity-0 ${
-        !isOpen ? '!translate-y-0 opacity-100' : 'translate-y-full opacity-0'
-      } delay-[5000ms] cursor-pointer`}
-      onMouseUp={askForPwa()}>
+      className={`cursor-pointer flex underline select-none align-center justify-center fixed w-full bottom-0 left-0 font-ProximaNovaBold text-center text-white p-1 bg-color-2 duration-300 translate-y-full opacity-0 ${
+        isOpen ? '!translate-y-0 !opacity-100' : '!translate-y-full !opacity-0'
+      } delay-[5000ms]`}
+      onClick={askForPwa}>
       <MdInstallMobile />
       {t('Add to Home Screen!')}
     </div>
