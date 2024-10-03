bab2ba61f32a8850d777a1f755c1ab340bf1bdde
Ahmed <86502590+AhmedNasser1010@users.noreply.github.com>
2024-10-03 20:34:40 +0300

    Delete orders_app/.next directory

--- orders_app/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/user/OrderSync/orders_app/src/app/layout\":[\"static/media/463dafcda517f24f-s.p.woff\",\"static/media/4473ecc91f70f139-s.p.woff\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
