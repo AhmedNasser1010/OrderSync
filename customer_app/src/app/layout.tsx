5f60af7c1a5b6f2114753aadc86debbfe3d41eef
ahmednasser1010 <ahmedn.coder@gmail.com>
2026-08-07 15:34:08 +0300

    feat(customer_app): rebrand fonts to Arabic Cairo + Alexandria

--- customer_app/src/app/layout.tsx
+++ customer_app/src/app/layout.tsx
@@ -1,65 +1,18 @@
 import type { Metadata, Viewport } from "next";
-import localFont from "next/font/local";
+import { Cairo, Alexandria } from "next/font/google";
 import "./globals.css";
 import StoreProvider from "./StoreProvider";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { SerwistProvider } from "./serwist";
 
-const GrotThin = localFont({
-  src: "./fonts/BasisGrotesquePro-Thin.ttf",
-  variable: "--font-grot-thin",
-  weight: "300",
+const CairoFont = Cairo({
+  subsets: ["arabic", "latin"],
+  variable: "--font-cairo",
 });
-const GrotReg = localFont({
-  src: "./fonts/BasisGrotesquePro-Regular.ttf",
-  variable: "--font-grot-reg",
-  weight: "400",
-});
-const GrotMed = localFont({
-  src: "./fonts/BasisGrotesquePro-Medium.ttf",
-  variable: "--font-grot-med",
-  weight: "500",
-});
-const GrotBold = localFont({
-  src: "./fonts/BasisGrotesquePro-Bold.ttf",
-  variable: "--font-grot-bold",
-  weight: "700",
-});
-const GrotBlack = localFont({
-  src: "./fonts/BasisGrotesquePro-Black.ttf",
-  variable: "--font-grot-black",
-  weight: "900",
-});
-const ProximaThin = localFont({
-  src: "./fonts/ProximaNovaCond-Thin.ttf",
-  variable: "--font-proxima-thin",
-  weight: "300",
-});
-const ProximaMed = localFont({
-  src: "./fonts/ProximaNovaCond-Medium.ttf",
-  variable: "--font-proxima-med",
-  weight: "500",
-});
-const ProximaSemiBold = localFont({
-  src: "./fonts/ProximaNovaCond-SemiBold.ttf",
-  variable: "--font-proxima-semibold",
-  weight: "600",
-});
-const ProximaBold = localFont({
-  src: "./fonts/ProximaNovaCond-Bold.ttf",
-  variable: "--font-proxima-bold",
-  weight: "700",
-});
-const ProximaBlack = localFont({
-  src: "./fonts/ProximaNovaCond-Black.ttf",
-  variable: "--font-proxima-black",
-  weight: "900",
-});
-const Beiruti = localFont({
-  src: "./fonts/Beiruti-VariableFont_wght.ttf",
-  variable: "--font-beiruti",
-  weight: "100 900",
+const AlexandriaFont = Alexandria({
+  subsets: ["arabic", "latin"],
+  variable: "--font-alexandria",
 });
 
 export const metadata: Metadata = {
@@ -107,7 +60,7 @@ export default function RootLayout({
       className="h-full antialiased"
     >
       <body
-        className={`${GrotThin.variable} ${GrotReg.variable} ${GrotMed.variable} ${GrotBold.variable} ${GrotBlack.variable} ${ProximaThin.variable} ${ProximaMed.variable} ${ProximaSemiBold.variable} ${ProximaBold.variable} ${ProximaBlack.variable} ${Beiruti.variable} min-h-full flex flex-col`}
+        className={`${CairoFont.variable} ${AlexandriaFont.variable} min-h-full flex flex-col`}
       >
         {process.env.NODE_ENV !== "production" && (
           <script
